4988e9201a1141b35477221a53bfbabf8785802c
Gildásio Zeth <gildasiogx@gmail.com>
2015-11-18 13:29:27 -0300

    Alterações

--- Python/2bm_1.py
+++ Python/2bm_1.py
@@ -1,21 +1,30 @@
 #20 25 85 40 25 90 25 40 55 40 25
-notas = raw_input()
+notas = "20 25 85 40 25 90 25 40 55 40"
 notas = notas.split(" ")
-for i in notas: i = int(i)
+
+for i in range(len(notas)):
+        notas[i] = int(notas[i])
+
 notas.sort()
+
 print notas
 
 def count(l):
-	c = 0
-	oldC = 0
-	for i in l:
-		if oldC <= c: oldC = c
-		c = 0
-		for e in l:
-			if i == e:
-				c+=1
-		if c >= oldC: 
-			counted = i
-	return counted
+        c = 0
+        oldC = 0
+        for i in l:
+                if c >= oldC:
+                        oldC = c
+                c = 0
+                for e in l:
+                        if i == e:
+                                c+=1
+                if c >= oldC: 
+                        counted = i
+                        print "Valor mais repetido:", counted
+                        print "Vezes repetida:", c
+        print "\n"
+        resultado = "Maior e mais repetido valor: %d" % counted
+        return resultado
 
-print "Maior nota com maior frequencia: ", count(notas)
+print count(notas)
