6cbf72f4e1170a2c8861e802898f6bad96c86aa5
Gildásio Filho <gildasiozeth@github.com>
2016-01-04 17:24:02 -0300

    Alterações

--- Java/23122015/exercicio16.java
+++ Java/23122015/exercicio16.java
@@ -3,41 +3,43 @@ import java.util.Scanner;
 public class exercicio16{
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
-		String ossos, filo, alim;
-		ossos = input.nextLine();
-		filo = input.nextLine();
-		alim = input.nextLine();
-		if (ossos == "vertebrado"){
-			if (filo == "ave"){
-				if (alim == "carnivoro"){
+		//String ossos, filo, alim;
+		
+		String ossos = input.nextLine();
+		String filo = input.nextLine();
+		String alim = input.nextLine();
+
+		if (ossos.equals("vertebrado")){
+			if (filo.equals("ave")){
+				if (alim.equals("carnivoro")){
 					System.out.println("aguia");
 				}
-				else if (alim == "onivoro"){
+				else if (alim.equals("onivoro")){
 					System.out.println("pomba");
 				}
 			}
-			else if (filo == "mamifero"){
-				if (alim == "onivoro"){
+			else if (filo.equals("mamifero")){
+				if (alim.equals("onivoro")){
 					System.out.println("homem");
 				}
-				else if (alim == "herbivoro"){
-					System.out.println("vaca");}
+				else if (alim.equals("herbivoro")){
+					System.out.println("vaca");
 				}
 			}
-		else if(ossos == "invertebrado"){
-			if (filo == "inseto"){
-				if (alim == "hematofago"){
+		}else if(ossos.equals("invertebrado")){
+			if (filo.equals("inseto")){
+				if (alim.equals("hematofago")){
 					System.out.println("pulga");
 				}
-				else if (alim == "herbivoro"){
+				else if (alim.equals("herbivoro")){
 					System.out.println("lagarta");
 				}
 			}
-			else if (filo == "anelideo"){
-				if (alim == "hematofago"){
+			else if (filo.equals("anelideo")){
+				if (alim.equals("hematofago")){
 			 		System.out.println("sanguessuga");
 				}
-				else if (alim == "onivoro"){
+				else if (alim.equals("onivoro")){
 					System.out.println("minhoca");
 				}
 			}
