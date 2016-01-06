61e0900d95f9772d0245f61e1b73cc568a1a918a
gildasiozeth <gildasiogx@gmail.com>
2016-01-06 16:53:58 -0300

    Alterações

--- Java/05012016/Conta.java
+++ Java/05012016/Conta.java
@@ -1,20 +1,37 @@
 class Conta{
-	int numero;
-	String titular;
-	String cpf;
+	private int numero;
+	private String titular;
+	private String cpf;
 	private double saldo;
-	String extrato = "Extrato:\n";
-
+	private String extrato;
+	
+	public Conta(){}
+	public Conta(int numero, String titular){
+		this.numero = numero;
+		this.titular = titular;
+		if(this.numero > 0){
+			extrato = "Extrato da conta " + this.numero + ":\n";
+		}
+	}
+	public Conta(int numero, String titular, double saldoInicial){
+		this.numero = numero;
+		this.titular = titular;
+		this.saldo = saldoInicial;
+		if(this.numero > 0){
+			extrato = "Extrato da conta " + this.numero + ":\n";
+		}
+	}
+	
 	public void depositar(double valor){
-		saldo += valor;
-		String info = valor + " foi depositado em " + numero + "\n";
-		extrato += info;}
+		this.saldo += valor;
+		String info = valor + " foi depositado em " + this.numero + "\n";
+		this.extrato += info;}
 
 	public boolean sacar(double valor){
 		if(valor <= saldo){
-			saldo -= valor;
-			String info = valor + " foi sacado de " + numero + "\n";
-			extrato += info;
+			this.saldo -= valor;
+			String info = valor + " foi sacado de " + this.numero + "\n";
+			this.extrato += info;
 			return true;
 		}
 		else{
@@ -27,11 +44,11 @@ class Conta{
 		}
 	}
 
-	public void extrato(){
-		System.out.println(extrato);
+	public String extrato(){
+		return extrato;
 	}
 
 	public String toString(){
-		return "Conta: " + numero + " // Saldo: " + saldo;
+		return "Conta: " + this.numero + " // Saldo: " + this.saldo;
 	}
 }
--- Java/05012016/TestaConta.java
+++ Java/05012016/TestaConta.java
@@ -1,21 +1,17 @@
 class TestaConta{
 	public static void main(String[] args){
-		Conta conta1 = new Conta();
-		conta1.titular = "Rogerio Silva";
-		conta1.numero = 123;
-
-		Conta conta2 = new Conta();
-		conta2.titular = "Valeria Costa";
-		conta2.numero = 124;
+		Conta conta1 = new Conta(1, "Rogerio da Silva");
+		Conta conta2 = new Conta(2, "Valeria Costa");
 
 		conta1.depositar(1000);
 		conta2.depositar(1000);
 
-		conta2.transferir(500, conta1);
+		conta2.transferir(600, conta1);
 
 		System.out.print(conta1 + "\n");
 		System.out.print(conta2 + "\n");
 
-		conta1.extrato();
+		System.out.println(conta1.extrato());
+		System.out.println(conta2.extrato());
 	}
 }
