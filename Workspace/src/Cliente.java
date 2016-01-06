d08911cf347ae5be62e68bfaa2340054dde1fa24
gildasiozeth <gildasiogx@gmail.com>
2016-01-06 19:34:41 -0300

    Alterações

---
+++ Workspace/src/Cliente.java
@@ -0,0 +1,34 @@
+public class Cliente {
+	private String nome;
+	private String cpf;
+	private String cidade;
+	
+	public Cliente(String titular, String cpf, String cidade) {
+		this.nome = titular;
+		this.cpf = cpf;
+		this.cidade = cidade;
+	}
+	
+	public String toString(){
+		return this.nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+}
