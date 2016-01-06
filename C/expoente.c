f579e3e3f6d3a3091653a0768b8ab5fe0a23f2a3
gildasiozeth <gildasiogx@gmail.com>
2016-01-06 13:47:14 -0300

    Alterações

--- C/expoente.c
+++ C/expoente.c
@@ -9,13 +9,8 @@ int expo(int x, int y){
 int main() {
 
     int x, y;
-    //do{
-    //printf("Digite x e y: ");
     scanf("%d %d", &x, &y);
-    //if(x==0 && y == 0){break;}
-    //else{printf("O resultado de %d elevado a %d: %d\n", x, y, expo(x, y));}
     printf("%d", expo(x, y));
-    //}while(1);
 
     return 0;
 }
