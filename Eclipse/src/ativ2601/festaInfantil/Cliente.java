c50c6f76416f44d930927abd83c23a7339f90ca0
gildasiozeth <gildasiogx@gmail.com>
2016-01-26 17:40:33 -0300

    Alterações

--- Eclipse/src/ativ2601/festaInfantil/Cliente.java
+++ Eclipse/src/ativ2601/festaInfantil/Cliente.java
@@ -4,40 +4,54 @@ public class Cliente {
 
 	public final static String NOVO = "Novo";
 	public final static String ANTIGO = "Antigo";
+	public final static int MAX_ALUGUEIS = 4;
 	
 	private String nome;
 	private String telefone;
 	private String endereco;
-	private Tema temaEscolhido;
-	private String dataFesta;
-	private String horaComecoFesta;
-	private String horaFinalFesta;
+	private Aluguel[] alugueis;
+	private int qtdMaxAlugueis = MAX_ALUGUEIS;
+	private int contadorAlugueis = 0;
 	private String tipo;
-	private double valorAluguel;
 	
 	public String toString() {
-		return "Nome: " + this.nome + "\nTema escolhido: " + this.temaEscolhido.getNome() + "\nValor do aluguel: " + this.valorAluguel + "\n";
+		return "Nome: " + this.nome + "\nQuantidade de alugueis: " + this.contadorAlugueis + "\nValor total de alugueis: " + getValorTotal() + "\n";
 	}
 	
-	
-	Cliente(String nome, String telefone, String endereco, Tema temaEscolhido, String dataFesta){
+	Cliente(String nome, String telefone, String endereco){
 		this.nome = nome;
 		this.telefone = telefone;
 		this.endereco = endereco;
-		this.temaEscolhido = temaEscolhido;
-		this.valorAluguel = temaEscolhido.getValor();
-		this.dataFesta = dataFesta;
 		this.tipo = NOVO;
+		this.alugueis = new Aluguel[MAX_ALUGUEIS];
 	}
 	
 	Cliente(Tema temaEscolhido){
 		this.tipo = ANTIGO;
-		this.temaEscolhido = temaEscolhido;
-		this.valorAluguel = temaEscolhido.getValor();
+		this.alugueis = new Aluguel[MAX_ALUGUEIS];
 	}
-
-	public double getValorAluguel() {
-		return valorAluguel;
+	
+	public void adicionarAluguel(Aluguel aluguel){
+		
+		this.alugueis[contadorAlugueis++] = aluguel;
+		
+		if (contadorAlugueis == qtdMaxAlugueis){
+			this.qtdMaxAlugueis += MAX_ALUGUEIS;
+			Aluguel[] temp = this.alugueis;
+			this.alugueis = new Aluguel[qtdMaxAlugueis];
+			for (int i = 0; i < temp.length; i++) {
+				this.alugueis[i] = temp[i];
+			}
+		}
+	}
+	
+	public double getValorTotal(){
+		double valorTotal = 0;
+		for (Aluguel aluguel : alugueis) {
+			if (aluguel != null){valorTotal += aluguel.getValorAluguel();}
+		}
+		if (this.tipo == ANTIGO){valorTotal *= 0.8;}
+		return valorTotal;
 	}
 	
 	public String getNome() {
@@ -62,38 +76,6 @@ public class Cliente {
 	
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
-	}
-	
-	public Tema getTemaEscolhido() {
-		return temaEscolhido;
-	}
-	
-	public void setTemaEscolhido(Tema temaEscolhido) {
-		this.temaEscolhido = temaEscolhido;
-	}
-	
-	public String getDataFesta() {
-		return dataFesta;
-	}
-	
-	public void setDataFesta(String dataFesta) {
-		this.dataFesta = dataFesta;
-	}
-	
-	public String getHoraComecoFesta() {
-		return horaComecoFesta;
-	}
-	
-	public void setHoraComecoFesta(String horaComecoFesta) {
-		this.horaComecoFesta = horaComecoFesta;
-	}
-	
-	public String getHoraFinalFesta() {
-		return horaFinalFesta;
-	}
-	
-	public void setHoraFinalFesta(String horaFinalFesta) {
-		this.horaFinalFesta = horaFinalFesta;
 	}
 	
 	public String getTipo() {
--- Eclipse/src/ativ2601/festaInfantil/TestaFesta.java
+++ Eclipse/src/ativ2601/festaInfantil/TestaFesta.java
@@ -7,9 +7,14 @@ public class TestaFesta {
 		Tema Cinderela = new Tema("Cinderela", "Azul clara");
 	    Item Castelo = new Item("Castelo", 250);
 	    Cinderela.adicionarItem(Castelo);
+	    
+	    Aluguel aluguel1 = new Aluguel(Cinderela, "26/01/2016", "18h", "22h");
 		
-		Cliente cliente1 = new Cliente("Julia", "123456789", "Rua Aquela La, numero 1234", Cinderela, "26/01/2016");
+		Cliente cliente1 = new Cliente("Julia", "123456789", "Rua Aquela La, numero 1234");
+		cliente1.adicionarAluguel(aluguel1);
 		Cliente cliente2 = new Cliente(Cinderela);
+		cliente2.adicionarAluguel(aluguel1);
+		cliente2.adicionarAluguel(aluguel1);
 		cliente2.setNome("Fulano");
 		
 		System.out.println(cliente1);
