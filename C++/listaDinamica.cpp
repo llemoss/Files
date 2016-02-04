2bb1ecf9111677b5e815a39f6c46a15fb4b38ab6
gildasiozeth <gildasiogx@gmail.com>
2016-02-04 15:46:29 -0300

    Alterações

--- C++/listaDinamica.cpp
+++ C++/listaDinamica.cpp
@@ -11,26 +11,222 @@ typedef struct elemento{
 	elemento *prox;
 }Elemento;
 
+Elemento *inicio, *fim, *input;
+
+void addInicio(int mat, char nome[20]){
+
+	input = (Elemento *)malloc(sizeof(Elemento));
+	input->mat=mat;
+	strcpy(input->nome, nome);
+	input->prox=NULL;
+	
+	if (inicio == NULL){
+		inicio = input;
+		fim = input;
+	}else{
+		input->prox = inicio;
+		inicio = input;
+	}
+}
+
+
+void addFinal(int mat, char nome[20]){
+
+	input = (Elemento *)malloc(sizeof(Elemento));
+	input->mat=mat;
+	strcpy(input->nome, nome);
+	input->prox=NULL;
+	
+	if (inicio == NULL){
+		inicio = input;
+		fim = input;
+	}else{
+		fim->prox = input;
+		fim = input;
+	}
+}
+
+void addElemento(int mat, char nome[20]){
+	
+	int entrou = 0;
+	
+	input = (Elemento *)malloc(sizeof(Elemento));
+	input->mat=mat;
+	strcpy(input->nome, nome);
+	input->prox = NULL;
+	
+	if (inicio == NULL){
+		inicio = input;
+		fim = input;
+	}else{
+		
+		Elemento *atual = inicio;
+		Elemento *anterior = NULL;
+		
+		while(atual != NULL){
+			if(input->mat < atual->mat){
+				if(atual == inicio){
+					input->prox = inicio;
+					inicio = input;
+					entrou = 1;
+				}else{
+					anterior->prox = input;
+					input->prox = fim;
+					entrou = 1;
+				}
+			}
+			anterior = atual;
+			atual = atual->prox;
+		}
+		
+		if (entrou == 0){
+			fim->prox = input;
+			fim = input;
+		}
+	}
+}
+
+void rmvElemento(int mat){
+	
+	Elemento *atual = inicio;
+	Elemento *anterior = NULL;
+	
+	while(true){
+		if(atual->mat == mat){
+			break;
+		}else{
+			anterior = atual;
+			atual = atual->prox;
+		}
+	}
+	
+	if(atual == inicio){
+		inicio = atual->prox;
+		atual = NULL;
+	}else{
+	anterior->prox = atual->prox;
+	atual = NULL;
+	}
+	
+}
+
+void buscar(int mat){
+	
+	Elemento *atual = inicio;
+	Elemento *anterior = NULL;
+	
+	int achou = 0;
+	
+	do{
+		if(atual->mat == mat){
+			achou = 1;
+			cout << "Resultado encontrado:\n" << "Nome: " << atual->nome << " // Mat: " << atual->mat << " // Prox: " << atual->prox << endl;
+			break;
+		}else{
+			anterior = atual;
+			atual = atual->prox;
+		}
+	}while(atual != NULL);
+	
+	if(achou == 0){
+		cout << "Resultado nao encontrado!" << endl;
+	}
+}
+
+Elemento *buscaRtn(int mat){
+	
+	Elemento *atual = inicio;
+	
+	while(atual != NULL){
+		if(atual->mat == mat){
+			return atual;
+			//break;
+		}
+		atual = atual->prox;
+	}
+	
+	return NULL;
+}
+
+void mostrarLista(){
+	Elemento *atual;
+	atual = inicio;
+	while(atual != NULL){
+		cout << "Nome: " << atual->nome << " // Mat: " << atual->mat << " // Prox: " << atual->prox << endl;
+		atual = atual->prox;
+	}
+	cout << endl;
+}
+
 int main(){
 	
-	Elemento *novo;
-	novo = (Elemento *)malloc(sizeof(Elemento));
-	novo->mat=1;
-	strcpy(novo->nome, "Joao");
-	novo->prox=0;
-	
-	Elemento *novo1;
-	novo1 = (Elemento *)malloc(sizeof(Elemento));
-	novo1->mat=2;
-	strcpy(novo1->nome, "Julia");
-	novo1->prox=NULL;
-	
-	novo->prox=novo1;
+	inicio = NULL;
+	fim = NULL;
 
-	cout << "Nome: " << novo->nome << " // Mat: " << novo->mat << " // Prox: " << novo->prox << endl;
-	cout << "Endereco do proximo elemento: " << novo1 << endl;
-	cout << "Nome: " << novo1->nome << " // Mat: " << novo1->mat << " // Prox: " << novo1->prox << endl;
-	cout << "Endereco do proximo elemento: " << novo1->prox;
+	addElemento(2, "Julia");
+	addElemento(1, "Gil");
+	addElemento(4, "Quarto");
+	addElemento(3, "Sexto");
+	
+	int opcao;
+	
+	do{
+		cout << "Opcoes:\n1 - Mostrar elementos\n2 - Adicionar elemento (ordenado)\n3 - Adicionar elemento no comeco\n4 - Adicionar elemento no final\n5 - Remover elemento\n6 - Buscar elemento\n0 - Sair\nDigite uma opcao: ";
+		int mat;
+		char nome[20];
+		Elemento *resultado;
+		cin >> opcao;
+		switch(opcao){
+		case 1:
+			system("cls");
+			mostrarLista();
+			break;
+		case 2:
+			cout << "Digite matricula e nome a serem adicionados: ";
+			cin >> mat >> nome;
+			addElemento(mat, nome);
+			system("cls");
+			cout << "Elemento adicionado com sucesso!\n";
+			mostrarLista();
+			break;
+		case 3:
+			cout << "Digite matricula e nome a serem adicionados: ";
+			cin >> mat >> nome;
+			addInicio(mat, nome);
+			system("cls");
+			cout << "Elemento adicionado com sucesso!\n";
+			mostrarLista();
+			break;
+		case 4:
+			cout << "Digite matricula e nome a serem adicionados: ";
+			cin >> mat >> nome;
+			addFinal(mat, nome);
+			system("cls");
+			cout << "Elemento adicionado com sucesso!\n";
+			mostrarLista();
+			break;
+		case 5:
+			cout << "Digite matricula a ser deletada: ";
+			cin >> mat;
+			resultado = buscaRtn(mat);
+			if (resultado == NULL){cout << "Matricula nao existe!\n"; break;}
+            else{rmvElemento(resultado->mat);
+			system("cls");
+			cout << "Elemento deletado com sucesso!\n";
+			mostrarLista();
+			break;}
+		case 6:
+			cout << "Digite matricula a ser buscada: ";
+			cin >> mat;
+			resultado = buscaRtn(mat);
+			system("cls");
+			if (resultado == NULL){cout << "Resultado nao encontrado!\n";}
+			else{cout << "Resultado:\n" << "Nome: " << resultado->nome << " // Mat: " << resultado->mat << " // Prox: " << resultado->prox << endl << endl;}
+			break;
+		default:
+			break;
+		}
+	}while(opcao!=0);
 	
 	return 0;
 }
